0e140e586b0a8b90f7188bcead7331f811f906e7
emerson <emerson.almeida@a.ficr.com.br>
2022-05-30 01:06:11 -0300

    criando models e executando migrate

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Curso
+
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ("titulo", "categoria", "vizualizacao", "data_criacao")
 
 # Register your models here.
+admin.site.register(Curso, CursoAdmin )
--- home/models.py
+++ home/models.py
@@ -1,3 +1,26 @@
+from django.utils import timezone
+from distutils.command.upload import upload
 from django.db import models
 
-# Create your models here.
+#? criando categoria cursos
+#? armazenar no banco - mostrar ao usuario 
+LISTA_CATEGORIAS =(
+    ("FRONT-END","FRONT-END"),
+    ("BACK-END","BACK_END"),
+    ("GITHUB","GIT - GITHUB"),
+    ("BANCO_DE_DADOS","BANCO DE DADOS"),
+    ("OUTROS", "OUTROS"),
+    ("INGLES", "INGLÊS"),
+) 
+
+#criar cursos
+class Curso(models.Model):
+    titulo = models.CharField(max_length=100)
+    thumb = models.ImageField(upload_to='thumb_cursos')
+    descicao = models.TextField(max_length=10000)
+    categoria = models.CharField(max_length=25, choices=LISTA_CATEGORIAS)
+    vizualizacao = models.IntegerField(default=0)
+    data_criacao = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return self.titulo
