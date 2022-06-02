6e22e5db11f449c8577e701e75c4f3fc5b18d44d
emerson <emerson.almeida@a.ficr.com.br>
2022-06-02 01:48:37 -0300

    Corrigindo erro de digitação

--- home/admin.py
+++ home/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Curso
 
 class CursoAdmin(admin.ModelAdmin):
-    list_display = ("titulo", "categoria", "vizualizacao", "data_criacao")
+    list_display = ("titulo", "categoria", "visualizacao", "data_criacao")
 
 # Register your models here.
 admin.site.register(Curso, CursoAdmin )
